5887b7ce2030e89e20d9a50316ca26c3bea77f23
CintiaNataly <164966730+CintiaNataly@users.noreply.github.com>
2024-09-28 18:28:45 -0300

    Actualización de archivos

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 
+    <link rel="shortcut icon" href="img/aero-asist-icon.png"/>
 
 </head>
 
